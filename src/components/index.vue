5a6a1e679940554e3e87a0e89753c2c5b7eafc6a
yuwanqin1 <1561193788@qq.com>
2019-06-11 15:33:23 +0800

    拉取项目测试-1

--- src/components/index.vue
+++ src/components/index.vue
@@ -41,8 +41,7 @@ export default class Index extends Vue {
       { name: '我的', icon: 'icon-user', type: 1 },
       { name: '设置', icon: 'icon-icon--', type: 2 },
       { name: '切换', icon: 'icon-qiehuanyonghu', type: 3 },
-      { name: '退出', icon: 'icon-tuichu', type: 4 },
-      { name: '修改密码', icon: 'icon-tuichu', type: 5 },
+      { name: '退出', icon: 'icon-tuichu', type: 4 }
     ]
   }
   clickMenu (data: any) {
